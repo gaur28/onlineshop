6fcd538280f087b354467870971c3e77f968bb08
Tushar Gaur <gaurtushar5567@gmail.com>
2023-02-23 13:40:27 +0530

    created a online shop project

---
+++ middlewares/check-auth.js
@@ -0,0 +1,15 @@
+function checkAuthStatus(req, res, next) {
+    const uid = req.session.uid;
+    
+    if (!uid  ) {
+      return next();
+    }
+    
+    res.locals.uid = uid;
+    res.locals.isAuth = true; 
+    res.locals.isAdmin = req.session.isAdmin; 
+    res.locals.isSeller = req.session.isSeller
+    next();
+  }
+  
+  module.exports = checkAuthStatus;
